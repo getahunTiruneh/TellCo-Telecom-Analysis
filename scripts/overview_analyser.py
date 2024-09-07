4f3b5650f09fa5d17661a1491c5e735105de9b1a
getahunTiruneh <gechtiru@gmail.com>
2024-09-07 22:04:36 +0300

    finalize overview analyser script

--- scripts/overview_analyser.py
+++ scripts/overview_analyser.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
 
 class UserOverviewAnalysis:
     def __init__(self,df):
@@ -14,6 +16,17 @@ class UserOverviewAnalysis:
         top_10_handsets=handset_counts.head(10)
         print("Top 10 Handsets Used by Customers:")
         return top_10_handsets.to_frame().reset_index()
+    def plot_top_10_handsets(self, df):
+        handset_counts = df['Handset Type'].value_counts()
+        top_10_handsets = handset_counts.head(10)
+
+        # Plot the top 10 handsets
+        top_10_handsets.plot(kind='bar', legend=False, color='skyblue')
+        plt.title('Top 10 Handsets Used by Customers')
+        plt.xlabel('Handset Type')
+        plt.ylabel('Number of Users')
+        plt.xticks(rotation=45, ha='right')
+        plt.show()
     def top_3_handset_manufacturers(self, df):
         """
         Plots the top 3 handset manufacturers based on the number of users.
@@ -79,4 +92,50 @@ class UserOverviewAnalysis:
             plt.tight_layout()
             plt.show()
 
+    def plot_univariate_unalysis(self, df):
+        # Univariate analysis: Total Duration Distribution
+        plt.figure(figsize=(10, 6))
+        sns.histplot(df['Total Duration'], bins=30, kde=True)
+        plt.title('Total Duration Distribution')
+        plt.show()
+    
+    def plot_bivariate_analysis(self, df):
+        # Bivariate analysis: Social Media DL vs Total Data
+        plt.figure(figsize=(10, 6))
+        sns.scatterplot(x='Social Media DL (Bytes)', y='Total Data', data=df)
+        plt.title('Social Media DL vs Total Data')
+        plt.show()
+    def corr_analysis(self, df):
+        # corrilation analysis for selected columns
+        app_data = df[['Social Media DL (Bytes)', 'Google DL (Bytes)', 'Email DL (Bytes)', 
+               'Youtube DL (Bytes)', 'Netflix DL (Bytes)', 'Gaming DL (Bytes)', 'Other DL (Bytes)']]
+        correlation_matrix = app_data.corr()
+        plt.figure(figsize=(10, 8))
+        sns.heatmap(correlation_matrix, annot=True)
+        plt.title('Correlation Matrix')
+        plt.show()
+    
+    def plot_PCA(self, df):
         
+        # Dimensionality Reduction
+        features = ['Social Media DL (Bytes)', 'Google DL (Bytes)', 'Email DL (Bytes)',
+                    'Youtube DL (Bytes)', 'Netflix DL (Bytes)', 'Gaming DL (Bytes)', 'Other DL (Bytes)']
+
+        x = df.loc[:, features].values
+        x = StandardScaler().fit_transform(x)
+
+        pca = PCA(n_components=2)
+        principalComponents = pca.fit_transform(x)
+        principalDf = pd.DataFrame(data=principalComponents, columns=['PC1', 'PC2'])
+
+        # Explained variance
+        explained_variance = pca.explained_variance_ratio_
+        print("Explained Variance by Principal Components:", explained_variance)
+        
+        # Plotting PCA
+        plt.figure(figsize=(8, 6))
+        plt.scatter(principalDf['PC1'], principalDf['PC2'])
+        plt.xlabel('Principal Component 1')
+        plt.ylabel('Principal Component 2')
+        plt.title('PCA of Application Data')
+        plt.show()
